9e410b9bb3bc5d8f91b2528365bdfb71dbbf4b9a
fabianodasilvalira <fabiano.lira@objetivopi.com.br>
2022-01-12 17:39:52 -0300

    cont commit

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,21 +1,28 @@
 const url = require('url')
+const UploadHandler = require('./uploadHandler')
+const { pipelineAsync, logger } = require('./util')
 class Routes {
     #io
     constructor(io) {
         this.#io = io
     }
+    async options(request, response) {
+        response.writeHead(204, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST'
+        })
+        response.end()
+    }
+    
 
     async post(request, response) {
         const { headers } = request
         const { query: { socketId } } = url.parse(request.url, true)
         const redirectTo = headers.origin
-        
-        this.#io.to(socketId).emit('file-uploaded', 5e9)
-        this.#io.to(socketId).emit('file-uploaded', 5e9)
-        this.#io.to(socketId).emit('file-uploaded', 5e9)
-        this.#io.to(socketId).emit('file-uploaded', 5e9)
 
-        const onFinish = (response, redirectTo) => {
+        const uploadHandler = new UploadHandler(this.#io, socketId)
+
+        const onFinish = (response, redirectTo) => () => {
             response.writeHead(303, {
                 Connection: 'close',
                 Location: `${redirectTo}?msg=Files uploaded with success!`
@@ -23,10 +30,21 @@ class Routes {
 
             response.end()
         }
-        setTimeout(() => {
-            return onFinish(response, headers.origin)
-            
-        }, 2000);
+
+        const busboyInstance = uploadHandler
+            .registerEvents(
+                headers,
+                onFinish(response, redirectTo)
+            )
+
+        await pipelineAsync(
+            request,
+            busboyInstance
+        )
+
+        logger.info('Request finished with success!')
+        
+
     }
 }
 
